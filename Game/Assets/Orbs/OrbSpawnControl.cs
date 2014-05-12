8329e85bf3a2cb29d5d6b551ecfbaa4b3c428eca
James Como <nadji_li@hotmail.com>
2014-05-12 15:46:31 -0400

    Updated Orb Spawn Script

--- Game/Assets/Orbs/OrbSpawnControl.cs
+++ Game/Assets/Orbs/OrbSpawnControl.cs
@@ -3,21 +3,25 @@ using System.Collections;
 
 public class OrbSpawnControl : MonoBehaviour {
 	public GameObject orbPrefab;
-	public float timer;
-	public int spawnerCoolDown;
-	public bool itemSpawned;
-	public int itemType;
+	int itemType;
+	/*
+	float timer;
+	int spawnerCoolDown;
+	bool itemSpawned;
+	*/
 
 	// Use this for initialization
 	void Start () {
-		timer = 0.0f;
+		itemType = 0;
+		/*timer = 0.0f;
 		spawnerCoolDown = 0;
 		itemSpawned = false;
-		itemType = 0;
+		*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		/* (Old Orb Spawn Code)
 		timer += Time.deltaTime;
 		if (!itemSpawned) {
 			spawnerCoolDown = Random.Range(3,15);
@@ -51,12 +55,35 @@ public class OrbSpawnControl : MonoBehaviour {
 				itemSpawned = false;
 				timer = 0.0f;
 				break;
-
 			}
 		}
 		if (timer > 10000.0f) {
 			timer = 0.0f;
 			Debug.Log("Timer Error");
+		}
+		*/
+	}
+
+	public void SpawnOrb(Vector3 spawnPosition) {
+		itemType = Random.Range(0, 4); //0:Left, 1:Up, 2:Right, 3:Down
+
+		switch(itemType) {
+		case 0:
+			GameObject redOrb = Instantiate (orbPrefab, spawnPosition, Quaternion.identity) as GameObject;
+			redOrb.renderer.material.color = Color.red;
+			break;
+		case 1:
+			GameObject greenOrb = Instantiate (orbPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
+			greenOrb.renderer.material.color = Color.green;
+			break;
+		case 2:
+			GameObject blueOrb = Instantiate (orbPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
+			blueOrb.renderer.material.color = Color.blue;
+			break;
+		case 3:				
+			GameObject yellowOrb = Instantiate (orbPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
+			yellowOrb.renderer.material.color = Color.yellow;
+			break;
 		}
 	}
 }
