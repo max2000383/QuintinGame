9bf966fa9c65b4cb47775fe3c160278cbe7c43da
James Como <nadji_li@hotmail.com>
2014-05-12 13:15:25 -0400

    More Orb Spawner stuff. Mainly scripts and scene changes

---
+++ Game/Assets/Scripts/OrbMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbMovement : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//gameObject.transform.position += Camera.main.transform.position * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, Camera.main.transform.position, Time.deltaTime * speed);
+	}
+}
--- Game/Assets/Scripts/OrbSpawnControl.cs
+++ Game/Assets/Scripts/OrbSpawnControl.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class OrbSpawnControl : MonoBehaviour {
+	public GameObject orbPrefab;
 
 	// Use this for initialization
 	void Start () {
-		GameObject orbPrefab;
+		Instantiate (orbPrefab, gameObject.transform.position, gameObject.transform.rotation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
