2af7d3e8bd5fe4fbc2fa0d4df01b715e693db62f
James Como <nadji_li@hotmail.com>
2014-05-12 12:04:34 -0400

    Added new orb scene, orb spawner script, and prefab as well as folders for each

---
+++ Game/Assets/Scripts/OrbSpawnControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbSpawnControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject orbPrefab;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
