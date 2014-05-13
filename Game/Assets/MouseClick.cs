41c44b16feddbe9e5a95efa0becc0925edd6314b
jazzgnat <jazzgnat@yahoo.com>
2014-05-13 17:30:49 -0400

    added a script for the orb prefab that makes them shoot towards the target on mouse click. Target is set to 0,0,0 right now and should be changed to player when it is added to the scene.

---
+++ Game/Assets/MouseClick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClick : MonoBehaviour {
+	bool clicked;
+
+	// Use this for initialization
+	void Start () {
+		clicked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (clicked)
+			transform.position = Vector3.Lerp (transform.position, new Vector3 (0, 0, 0), 0.5f * Time.deltaTime * 15); 
+				Destroy (gameObject, 1.5f);
+	}
+
+	void OnMouseDown(){
+		Debug.Log ("Click");
+		if(gameObject.GetComponent<OrbControllerMax> ().collected){
+		gameObject.GetComponent<OrbControllerMax> ().collected = false;
+		gameObject.GetComponent<OrbControllerMax> ().hitMarker = false;
+		clicked = true;}
+	}
+}
+
