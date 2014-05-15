a955d6b45409f56b74a76851bdcecc0636810a2e
jazzgnat <jazzgnat@yahoo.com>
2014-05-15 12:59:20 -0400

    ring prefab and warpcore obstacle

---
+++ Game/Assets/RingCollect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingCollect : MonoBehaviour {
+
+	private Component[] scripts;
+	// Use this for initialization
+	void Start () {
+		scripts = GetComponentsInChildren<OrbControllerMax> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider myCol){
+		if (myCol.gameObject.tag == "Player") {
+			foreach (OrbControllerMax orb in scripts ){
+				orb.collected = true;
+			}
+		}
+	}
+}
