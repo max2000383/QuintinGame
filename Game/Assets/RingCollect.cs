01a2d9981fab49132bde7e1f951fde38fc7f6712
jazzgnat <jazzgnat@yahoo.com>
2014-05-16 12:07:22 -0400

    ring prefab and script changes. ring script makes random color orbs and collects on trigger with player

--- Game/Assets/RingCollect.cs
+++ Game/Assets/RingCollect.cs
@@ -4,20 +4,22 @@ using System.Collections;
 public class RingCollect : MonoBehaviour {
 
 	private Component[] scripts;
+	public float speed = 1.0f;
 	// Use this for initialization
 	void Start () {
-		scripts = GetComponentsInChildren<OrbControllerMax> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position=new Vector3(transform.position.x,Time.deltaTime*speed+transform.position.y,transform.position.z);
 	}
 
 	void OnTriggerEnter(Collider myCol){
+		scripts = GetComponentsInChildren<RingOrbInstantiate> ();
 		if (myCol.gameObject.tag == "Player") {
-			foreach (OrbControllerMax orb in scripts ){
-				orb.collected = true;
+			foreach (RingOrbInstantiate location in scripts ){
+				location.orb.GetComponent<OrbControllerMax>().collected = true;
 			}
 		}
 	}
--- Game/Assets/RingOrbInstantiate.cs
+++ Game/Assets/RingOrbInstantiate.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class RingOrbInstantiate : MonoBehaviour {
 	public GameObject orbPrefab;
 	private int itemType;
+	public GameObject orb;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,18 +15,22 @@ public class RingOrbInstantiate : MonoBehaviour {
 		case 0:
 			GameObject redOrb = Instantiate (orbPrefab, transform.position, Quaternion.identity) as GameObject;
 			redOrb.renderer.material.color = Color.red;
+			orb = redOrb;
 			break;
 		case 1:
 			GameObject greenOrb = Instantiate (orbPrefab, transform.position, Quaternion.identity) as GameObject;
 			greenOrb.renderer.material.color = Color.green;
+			orb = greenOrb;
 			break;
 		case 2:
 			GameObject blueOrb = Instantiate (orbPrefab, transform.position, Quaternion.identity) as GameObject;
 			blueOrb.renderer.material.color = Color.blue;
+			orb = blueOrb;
 			break;
 		case 3:				
 			GameObject yellowOrb = Instantiate (orbPrefab, transform.position, Quaternion.identity) as GameObject;
 			yellowOrb.renderer.material.color = Color.yellow;
+			orb = yellowOrb;
 			break;
 		}
 	
@@ -32,6 +38,5 @@ public class RingOrbInstantiate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
