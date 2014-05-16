2555487c8a8f04ea919777647036d107949eef18
jazzgnat <jazzgnat@yahoo.com>
2014-05-16 09:29:27 -0400

    orb rings with random colors and Asteroid models from Kennedy. Copy of current main scene for James

---
+++ Game/Assets/RingOrbInstantiate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingOrbInstantiate : MonoBehaviour {
+	public GameObject orbPrefab;
+	private int itemType;
+
+	// Use this for initialization
+	void Start () {
+		itemType = Random.Range(0, 4); //0:Left, 1:Up, 2:Right, 3:Down
+		
+		switch(itemType) {
+		case 0:
+			GameObject redOrb = Instantiate (orbPrefab, transform.position, Quaternion.identity) as GameObject;
+			redOrb.renderer.material.color = Color.red;
+			break;
+		case 1:
+			GameObject greenOrb = Instantiate (orbPrefab, transform.position, Quaternion.identity) as GameObject;
+			greenOrb.renderer.material.color = Color.green;
+			break;
+		case 2:
+			GameObject blueOrb = Instantiate (orbPrefab, transform.position, Quaternion.identity) as GameObject;
+			blueOrb.renderer.material.color = Color.blue;
+			break;
+		case 3:				
+			GameObject yellowOrb = Instantiate (orbPrefab, transform.position, Quaternion.identity) as GameObject;
+			yellowOrb.renderer.material.color = Color.yellow;
+			break;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
