38ceddfb6108075c3bec54bf070670eb50ab0f99
James Como <nadji_li@hotmail.com>
2014-05-20 14:51:03 -0400

    Restored Obstacle Script

---
+++ Game/Assets/Obstacle/ObstacleScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleScript : MonoBehaviour {
+	public float speed=1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		transform.position=new Vector3(transform.position.x,Time.deltaTime*speed+transform.position.y,transform.position.z);
+		
+		if(transform.position.y>6)Destroy(gameObject);
+	}
+}
